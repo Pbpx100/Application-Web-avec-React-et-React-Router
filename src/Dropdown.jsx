8fc3e41824c5a5c4e3249ff88229a12f1e1e7186
Paul Bonoso <pbonoso@drv.es>
2024-05-11 14:27:19 +0200

    third commit

---
+++ src/Dropdown.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useState } from 'react';
+import style from '../styles/Dropdown.module.css';
+import ToggleButton from './ToggleButton';
+
+
+const DropDown = ({ title, paragraphe, wrapClassName }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleOnClick = () => {
+        setIsOpen(!isOpen);
+    };
+
+    let display;
+    let arrowDirection;
+    if (isOpen) {
+        display = 'block';
+        arrowDirection = 'down';
+    } else {
+        display = 'none';
+        arrowDirection = 'up';
+    }
+
+
+
+    return (
+        <div
+            className={`${style.dropdownWrap} ${wrapClassName}`}
+            onClick={handleOnClick}
+        >
+            <div className={style.dropdownHeader}>
+                <span>{title}</span>
+                <i className={`fa-solid fa-chevron-${arrowDirection}`} />
+            </div>
+            <div className={style.dropdownBox} style={{ display }}>
+                <span className={style.paragraphe}>{paragraphe}</span>
+            </div>
+        </div>
+    );
+};
+
+export default DropDown;
