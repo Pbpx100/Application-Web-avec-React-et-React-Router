1dff4b539adff605cffc98a2b3cdaed49b193828
Paul Bonoso <pbonoso@drv.es>
2024-05-11 15:14:21 +0200

    eith commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './assets/App.css';
 import ReactDOM from 'react-dom/client'
-import Accueil from './Accueil.jsx'
 import Homepage from './Homepage.jsx';
 import Header from './components/Header.jsx';
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom'
@@ -10,6 +9,8 @@ import './index.css';
 import Footer from './components/Footer.jsx';
 import Error404 from './pages/Error404.jsx';
 import About from './pages/About.jsx';
+import './App.css'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -19,7 +20,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Header />
       <Routes>
         <Route path='/' element={<Homepage />} />
-        <Route path='/accueil' element={<Accueil />} />
         <Route path="/about" element={<About />} />
         <Route path="location/:locationId" element={<Location />} />
         <Route path="*" element={<Error404 />} />
